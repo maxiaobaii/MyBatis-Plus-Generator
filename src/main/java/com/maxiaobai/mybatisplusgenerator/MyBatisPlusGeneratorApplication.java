1d26bc11f9a94390d7af497de05bfd8f0df5d4de
v-shijintao <sjtao0314@163.com>
2021-09-04 19:35:04 +0800

    构建项目

---
+++ src/main/java/com/maxiaobai/mybatisplusgenerator/MyBatisPlusGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.maxiaobai.mybatisplusgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisPlusGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisPlusGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maxiaobai/mybatisplusgenerator/MyBatisPlusGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxiaobai.mybatisplusgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisPlusGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
